d055f18a3ca66179cb64d7e958e04eb1e8da9c4e
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-03-13 08:24:21 -0300

    :sparkles: new feature

--- exercicios/ejs/server.js
+++ exercicios/ejs/server.js
@@ -4,7 +4,26 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res){
-    res.render("pages/index");
+    const items = [
+        {
+        title: "E",
+        message: "specialmente fácil"
+        },
+        {
+            title: "J",
+            message: "ogos podem ser construídos"
+        },
+        {
+            title: "S",
+            message: "implicidade do código"
+        }
+        
+    ];
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML utilizando JS"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get("/sobre", function (req, res){
